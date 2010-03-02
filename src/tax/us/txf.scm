kleopatra999/gnucash-svn

commit e139108496592616a1e654aec2d89302895764e9

Bug #609043: Improve (mostly german) translation of txf
Patch by Frank H. Ellenberger:

This patch is a first extract of so an approach, which I have here, which will
probably lead to a german income tax declaration ESt-A1. But this extract is
more general and based on r18413 changes.

Changes:
1. Header Comments: adding Contributors
2. Most strings in de_DE translated to german.

git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@18673 57a11ea4-9604-0410-9ed3-97b8803252fd

src/tax/us/txf.scm
@@ -70,7 +70,7 @@
   (let ((pair (assv code txf-help-strings)))
     (if pair
         (cdr pair)
-        "No help available.")))
+        (_ "No help available.") )))
 
 (define (gnc:txf-get-codes categories tax-entity-type)
   (let ((tax-entity-codes (cdr (assv (string->symbol tax-entity-type)

src/tax/us/txf-de_DE.scm
@@ -3,8 +3,14 @@
 ;; This file was copied from the file txf.scm by  Richard -Gilligan- Uschold
 ;;
 ;; Originally, these were meant to hold the codes for the US tax TXF
-;; format. I modified this heavily so that it might become useful for
+;; format. Christian Stimming modified this heavily so that it might become useful for
 ;; the German Umsatzsteuer-Voranmeldung. 
+;; Further modifications by:
+;;   Jannick Asmus
+;;   J. Alex Aycinena
+;;   Frank H. Ellenberger
+;;   Andreas Köhler
+;;   Rolf Leggewie
 ;; 
 ;; This file holds all the Kennzahlen for the
 ;; Umsatzsteuer-Voranmeldung and their explanations, which can be
@@ -19,7 +25,7 @@
 (define txf-tax-entity-types
   (list
    (cons 'Ind #("Individual, Joint, etc." "Files Individual German Tax Return"))
-   (cons 'Other #("None" "No Income Tax Options Provided"))))
+   (cons 'Other #("None" "Keine Steuerberichtsoptionen vorgesehen"))))
 
 (define (gnc:tax-type-txf-get-code-info tax-entity-types type-code index)
   (let ((tax-entity-type (assv type-code tax-entity-types)))
@@ -66,7 +72,7 @@
   (let ((pair (assv code txf-help-strings)))
     (if pair
         (cdr pair)
-        "No help available.")))
+        (_ "No help available.") )))
 
 (define (gnc:txf-get-codes categories tax-entity-type)
   (let* ((tax-entity-code-list-pair (assv (if (eqv? tax-entity-type "")
@@ -94,9 +100,9 @@
 
 (define txf-help-categories
   (list
-   (cons 'H000 #(current "help" "Name of Current account is exported." 0 #f ""))
-   (cons 'H002 #(parent "help" "Name of Parent account is exported." 0 #f ""))
-   (cons 'H003 #(not-impl "help" "Not implemented yet, Do NOT Use!" 0 #f ""))))
+   (cons 'H000 #(current "help" "Name des aktuellen Kontos wird exportiert." 0 #f ""))
+   (cons 'H002 #(parent "help" "Name des übergeordneten Kontos wird exportiert." 0 #f ""))
+   (cons 'H003 #(not-impl "help" "Noch nicht implementiert, NICHT benutzen!" 0 #f ""))))
 
 ;; We use several formats; nr. 1 means Euro+Cent, nr. 2 means only full Euro
 

src/tax/us/txf-help-de_DE.scm
@@ -3,19 +3,22 @@
 ;; This file was copied from the file txf.scm by  Richard -Gilligan- Uschold
 ;;
 ;; Originally, these were meant to hold the codes for the US tax TXF
-;; format. I modified this heavily so that it might become useful for
+;; format. Christian Stimming modified this heavily so that it might become useful for
 ;; the German Umsatzsteuer-Voranmeldung. 
+;; Further modifications by:
+;;   Jannick Asmus
+;;   Frank H. Ellenberger
+;;   Rolf Leggewie
 ;;
 ;; This file holds the explanations to the categories from txf-de_DE.scm.
 ;;
 
 (define txf-help-strings
   '(
-    (H001 . "Categories marked with a \"&lt;\" or a \"^\", require a Payer identification to be exported.  \"&lt;\" indicates that the name of this account is exported as this Payer ID.  Typically, this is a bank, stock, or mutual fund name.")
-    (H002 . "Categories marked with a \"&lt;\" or a \"^\", require a Payer identification to be exported.  \"^\" indicates that the name of the PARENT of this account is exported as this Payer ID.  Typically, this is a bank, stock, or mutual fund name.")
-    (H003 . "Categories marked with a \"#\" are not fully implemented yet!  Do not use these codes!")
-    (N000 . "This is a dummy category and only shows up on the tax report, but is not exported.")
-
+    (H001 . "Bei mit \"&lt;\" oder \"^\" markierten Kategorien wird neben dem Betrag eine Beschreibung exportiert.  \"&lt;\" indiziert, daß der Name von diesem Konto als Beschreibung exportiert wird.  Genaueres kann dem jeweiligen Formular entnommen werden.")
+    (H002 . "Bei mit \"&lt;\" oder \"^\" markierten Kategorien wird neben dem Betrag eine Beschreibung exportiert.  \"^\" indiziert, daß der Name des übergeordneten Kontos als Beschreibung exportiert wird.  Genaueres kann dem jeweiligen Formular entnommen werden.")
+    (H003 . "Mit \"#\" markierte Kategorien sind noch nicht vollständig implementiert!  Bitte diese Codes nicht verwenden!")
+    (N000 . "Dies ist eine Pseudokategorie, welche zwar im Bericht angezeigt, aber nicht exportiert wird.")
 
     (K41 . "Innergemeinschaftliche Lieferungen (§ 4 Nr. 1 Buchst. b UStG) an Abnehmer mit USt-IdNr.  (Bemessungsgrundlage)")
     (K44 . "Innergemeinschaftliche Lieferungen neuer Fahrzeuge an Abnehmer ohne USt-IdNr (Bemessungsgrundlage)")

commit 2d6efa10ad549eec702926b9da09d52b2aa9a037

Fix crash on opening the tax report, introduced by r18673.
Patch by Alex Aycinena.

git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@18783 57a11ea4-9604-0410-9ed3-97b8803252fd

src/tax/us/txf.scm
@@ -17,6 +17,8 @@
 ;; Added 'None' type for no income tax options
 ;;
 
+(use-modules (gnucash app-utils))
+
 (define txf-tax-entity-types
   (list
    (cons 'F1040 #("Individual, Joint, etc." "Files US Form 1040 Tax Return"))